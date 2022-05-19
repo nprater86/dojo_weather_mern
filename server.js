9343881591f9e440149163153dada50e4738ae9d
Nathan Prater <nprater86@gmail.com>
2022-05-19 14:21:49 -0500

    updated position of cookies

--- server.js
+++ server.js
@@ -2,18 +2,15 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = 8000;
-
-//ENV
 require('dotenv').config();
-
-//cookie parser
 const cookieParser = require('cookie-parser');
-app.use(cookieParser());
-app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
+
 
 require("./server/config/mongoose.config");
 
 app.use(express.json(), express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
 
 const AllUserRoutes = require("./server/routes/user.routes");
 AllUserRoutes(app);
